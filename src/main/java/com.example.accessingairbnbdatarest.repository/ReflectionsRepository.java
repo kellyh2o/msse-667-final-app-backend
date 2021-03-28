e2485d91c7e36c7665ad3863429ce453c6785b0c
Kelly Watermeyer <kellywatermeyer@gmail.com>
2021-03-28 07:34:46 -0700

    update endpoints to reflections

--- src/main/java/com.example.accessingairbnbdatarest.repository/ReflectionsRepository.java
+++ src/main/java/com.example.accessingairbnbdatarest.repository/ReflectionsRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "listingsAndReviews", path = "listingsAndReviews")
+@RepositoryRestResource(collectionResourceRel = "reflections", path = "reflections")
 public interface ReflectionsRepository extends MongoRepository<Reflection, String> {
 
     List<Reflection> findByName(@Param("name") String name);
